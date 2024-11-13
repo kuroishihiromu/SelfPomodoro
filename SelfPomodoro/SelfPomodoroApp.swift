036c3b8e9dbf0df7d2b959bb2ee1d40bd60d5e5d
kuroishihiromu <kuroishihiromu@gmail.com>
2024-11-13 17:15:18 +0900

    Initial Commit

---
+++ SelfPomodoro/SelfPomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfPomodoroApp.swift
+//  SelfPomodoro
+//
+//  Created by 黒石陽夢 on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct SelfPomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SelfPomodoroTests/SelfPomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  SelfPomodoroTests.swift
+//  SelfPomodoroTests
+//
+//  Created by 黒石陽夢 on 2024/11/13.
+//
+
+import Testing
+@testable import SelfPomodoro
+
+struct SelfPomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
