d8c39ad7f1da90ed9992228ab87ef59b3b8adb7c
yoshidashuya <lemon.sy.1221@gmail.com>
2025-02-24 13:53:22 +0900

    feat: HomeViewにタイマー設定モダールを表示するボタンを追加

--- SelfPomodoro/Views/HomeView.swift
+++ SelfPomodoro/Views/HomeView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showSettings = false  // モーダル表示の有無を管理する状態変数
+
     var body: some View {
-        VStack{
+        ZStack {
+            VStack {
+                Spacer()
+                Button(action: {
+                    showSettings = true
+                }) {
+                    Text("Start")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color(.blue))
+                        .cornerRadius(10)
+                }
+                Spacer()
+            }
+        }
+        .sheet(isPresented: $showSettings) {  // スタートボタンが押されたときにモーダルを表示
+//            TimerSettingsView()
         }
     }
 }
