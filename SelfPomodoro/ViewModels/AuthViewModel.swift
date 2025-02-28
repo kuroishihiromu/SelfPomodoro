5557734d794a1e7ce3039cdf584ee0464b0f0ea7
kuroishihiromu <kuroishihiromu@gmail.com>
2025-02-28 13:37:38 +0900

    feat: 認証用ViewModelの作成

--- SelfPomodoro/ViewModels/AuthViewModel.swift
+++ SelfPomodoro/ViewModels/AuthViewModel.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+import Supabase
+import SwiftUI
+
+@MainActor
+class AuthViewModel: ObservableObject {
+    @Published var email = ""
+    @Published var password = ""
+    @Published var isLoading = false
+    @Published var result: Result<Void, Error>?
+    @Published var isAuthenticated = false // 認証状態を管理
+
+    
+    init() {
+        Task {
+            await observeAuthChanges()
+        }
+    }
+    
+    func signIn() async {
+        isLoading = true
+        defer { isLoading = false }
+
+        do {
+            try await supabase.auth.signIn(email: email, password: password)
+            DispatchQueue.main.async {
+                self.isAuthenticated = true
+            }
+            result = .success(())
+        } catch {
+            result = .failure(error)
+        }
+    }
+
+    func signUp() async {
+        isLoading = true
+        defer { isLoading = false }
+        
+        do {
+            try await supabase.auth.signUp(email: email, password: password)
+            DispatchQueue.main.async {
+                self.isAuthenticated = true
+            }
+            result = .success(())
+        } catch {
+            print("SignUp Error:", error.localizedDescription) 
+            result = .failure(error)
+        }
+    }
+    
+    func observeAuthChanges() async {
+        for await state in supabase.auth.authStateChanges {
+            print("Auth State Changed: \(state.event), Session: \(String(describing: state.session))")
+
+            DispatchQueue.main.async {
+                if state.event == .initialSession {
+                    print("Skipping initial session check")
+                    return
+                }
+                self.isAuthenticated = state.session != nil
+                print("Updated isAuthenticated: \(self.isAuthenticated)")
+            }
+        }
+    }
+}
+
