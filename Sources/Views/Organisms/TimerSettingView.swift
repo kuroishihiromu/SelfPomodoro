0e1de12ee0d52c0a0e298e7e82193eabd3ba04e7
kuroishihiromu <kuroishihiromu@gmail.com>
2025-03-20 14:14:22 +0900

    init: 空ファイルを用意

---
+++ Sources/Views/Organisms/TimerSettingView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerSettingView.swift
+//  SelfPomodoro
+//
+//  Created by 黒石陽夢 on 2025/03/20.
+//
+
+import SwiftUI
+
+struct TimerSettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerSettingView()
+}
---
+++ Sources/Views/Pages/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  SelfPomodoro
+//
+//  Created by 黒石陽夢 on 2025/03/20.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
