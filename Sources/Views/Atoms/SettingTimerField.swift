bdd38ccffbf1f0118dee415e606db06d41ac677b
yoshidashuya <lemon.sy.1221@gmail.com>
2025-03-26 20:28:21 +0900

    feat: 時間入力コンポーネントを実装

--- Sources/Views/Atoms/SettingTimerField.swift
+++ Sources/Views/Atoms/SettingTimerField.swift
@@ -8,11 +8,58 @@
 import SwiftUI
 
 struct SettingTimerField: View {
+    let placeholder: String
+    let width: CGFloat
+    let height: CGFloat
+    @Binding var text: String
+
+    private let bgColor = ColorTheme.white
+    private let borderColor = ColorTheme.Gray
+    private let fontColor = ColorTheme.black
+
+    init(
+        placeholder: String,
+        width: CGFloat,
+        height: CGFloat,
+        text: Binding<String>
+    ) {
+        self.placeholder = placeholder
+        self.width = width
+        self.height = height
+        self._text = text
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            TextField(placeholder, text: $text)
+                .foregroundColor(fontColor)
+                .keyboardType(.numberPad)
+        }
+        .padding()
+        .frame(width: width, height: height)
+        .background(bgColor)
+        .overlay(
+            RoundedRectangle(cornerRadius: 6)
+                .stroke(borderColor)
+        )
     }
 }
 
 #Preview {
-    SettingTimerField()
+    PreviewWrapperSettingTimerField()
+}
+
+struct PreviewWrapperSettingTimerField: View {
+    @State var time: String = ""
+
+    var body: some View {
+        VStack {
+            SettingTimerField(
+                placeholder: "25",
+                width: 160,
+                height: 44,
+                text: $time
+            )
+        }
+    }
 }
