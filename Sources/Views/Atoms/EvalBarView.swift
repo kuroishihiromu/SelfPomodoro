ccf81f8748ed89d4caf1480b52d69aae3c9bd9d1
yoshidashuya <lemon.sy.1221@gmail.com>
2025-03-29 15:16:06 +0900

    add: EvalBarView.swiftファイルを作成

---
+++ Sources/Views/Atoms/EvalBarView.swift
@@ -0,0 +1,18 @@
+//
+//  EvalBarView.swift
+//  SelfPomodoro
+//
+//  Created by し on 2025/03/29.
+//
+
+import SwiftUI
+
+struct EvalBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EvalBarView()
+}
