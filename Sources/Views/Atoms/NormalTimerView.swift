080e288407adf4943a9f255b38a0333f0ca099b9
yoshidashuya <lemon.sy.1221@gmail.com>
2025-03-30 12:57:07 +0900

    add: NormalTimerView.swiftファイルを作成

---
+++ Sources/Views/Atoms/NormalTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  NormalTimerView.swift
+//  SelfPomodoro
+//
+//  Created by し on 2025/03/30.
+//
+
+import SwiftUI
+
+struct NormalTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NormalTimerView()
+}
