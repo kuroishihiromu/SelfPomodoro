aed8a0bcb9f9b4a4af04d116bf451b133f4e0279
kuroishihiromu <kuroishihiromu@gmail.com>
2025-04-07 01:17:48 +0900

    fix: 命名の修正

--- Sources/Views/Organisms/TabBarView.swift
+++ Sources/Views/Organisms/TabBarView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
-
 struct TabBarView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            HomeScreenView()
+                .tabItem {
+                    Image(systemName: "house")
+                }
+
+            TaskManagementScreenView()
+                .tabItem {
+                    Image(systemName: "checkmark.circle")
+                }
+
+            StatisticsScreenView()
+                .tabItem {
+                    Image(systemName: "chart.bar")
+                }
+
+            ProfileScreenView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle")
+
+                }
+        }
     }
 }
 
--- Sources/Views/Pages/TaskManagementScreenView.swift
+++ Sources/Views/Pages/TaskManagementScreenView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct TaskManagementScreen: View {
+struct TaskManagementScreenView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TaskManagementScreen()
+    TaskManagementScreenView()
 }
