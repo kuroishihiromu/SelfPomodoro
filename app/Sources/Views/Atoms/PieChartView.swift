0f9f0212579d92a8e9f64208408f9eb1cffad351
yoshidashuya <lemon.sy.1221@gmail.com>
2025-04-19 11:57:00 +0900

    add: PieChartViewファイルを追加

---
+++ app/Sources/Views/Atoms/PieChartView.swift
@@ -0,0 +1,18 @@
+//
+//  PieChartView.swift
+//  SelfPomodoro
+//
+//  Created by し on 2025/04/19.
+//
+
+import SwiftUI
+
+struct PieChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PieChartView()
+}
