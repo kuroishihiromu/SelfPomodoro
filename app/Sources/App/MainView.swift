a3f56bdf874b8f843bb5ef119761fac9f0b73ddc
kuroishihiromu <kuroishihiromu@gmail.com>
2025-04-23 23:49:34 +0900

    feat: MainViewでTimerScreenViewを呼び出す

--- app/Sources/App/MainView.swift
+++ app/Sources/App/MainView.swift
@@ -14,24 +14,40 @@ struct MainView: View {
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
             VStack(spacing: 0) {
-                // タブによって画面切り替え
                 Group {
                     switch viewStore.selectedTabIndex {
-                    case 0: TimerScreenView()
-                    case 1: TaskManagementScreenView()
-                    case 2: StatisticsScreenView()
-                    case 3: ProfileScreenView()
-                    default: EmptyView()
+                    case 0:
+                        TimerScreenView(
+                            store: Store(
+                                initialState: TimerScreenFeature.State(
+                                    timer: TimerFeature.State(
+                                        totalSeconds: 5,
+                                        taskDuration: 5,
+                                        restDuration: 5
+                                    ),
+                                    todoList: ToDoListFeature.State(),
+                                    evalModal: nil
+                                ),
+                                reducer: { TimerScreenFeature() }
+                            )
+                        )
+                    case 1:
+                        TaskManagementScreenView()
+                    case 2:
+                        StatisticsScreenView()
+                    case 3:
+                        ProfileScreenView()
+                    default:
+                        EmptyView()
                     }
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
-                // タブバーを表示
+
                 TabBarView(store: store)
             }
         }
     }
 }
-
 #Preview {
     MainView(store: Store(initialState: TabButtonFeature.State()) {
         TabButtonFeature()
