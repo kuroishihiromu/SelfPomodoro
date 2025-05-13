8ff48bb3b24b62937204df1b0ca60dd7817013fb
tsunakit99 <tainosuke0926@gmail.com>
2025-05-13 23:58:02 +0900

    feat: すべてのリポジトリをまとめたファクトリ実装

---
+++ backend/internal/infrastructure/repository/repository_factory.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"github.com/tsunakit99/selfpomodoro/internal/domain/repository"
+	"github.com/tsunakit99/selfpomodoro/internal/infrastructure/database"
+	"github.com/tsunakit99/selfpomodoro/internal/infrastructure/logger"
+	"github.com/tsunakit99/selfpomodoro/internal/infrastructure/repository/postgres"
+)
+
+// RepositoryFactory はすべてのリポジトリを管理するファクトリ
+type RepositoryFactory struct {
+	Task repository.TaskRepository
+	// TODO: 他のリポジトリを追加する場合はここにフィールドを追加
+}
+
+// NewRepositoryFactory はすべてのリポジトリを初期化する
+func NewRepositoryFactory(postgresDB *database.PostgresDB, dynamoDB *database.DynamoDB, logger logger.Logger) *RepositoryFactory {
+	// PostgresDBを使用してリポジトリを初期化
+	taskRepo := postgres.NewTaskRepository(postgresDB, logger)
+
+	// TODO: DynamoDBを使用してリポジトリを初期化する場合はここに追加
+
+	return &RepositoryFactory{
+		Task: taskRepo,
+	}
+}
