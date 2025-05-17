471efa55ce0cb958feb97a131935dec1fdbd28aa
tsunakit99 <tainosuke0926@gmail.com>
2025-05-17 22:44:57 +0900

    add: ファクトリにSession関連を追加

--- backend/internal/infrastructure/repository/repository_factory.go
+++ backend/internal/infrastructure/repository/repository_factory.go
@@ -9,7 +9,8 @@ import (
 
 // RepositoryFactory はすべてのリポジトリを管理するファクトリ
 type RepositoryFactory struct {
-	Task repository.TaskRepository
+	Task    repository.TaskRepository
+	Session repository.SessionRepository
 	// TODO: 他のリポジトリを追加する場合はここにフィールドを追加
 }
 
@@ -17,10 +18,12 @@ type RepositoryFactory struct {
 func NewRepositoryFactory(postgresDB *database.PostgresDB, dynamoDB *database.DynamoDB, logger logger.Logger) *RepositoryFactory {
 	// PostgresDBを使用してリポジトリを初期化
 	taskRepo := postgres.NewTaskRepository(postgresDB, logger)
+	sessionRepo := postgres.NewSessionRepository(postgresDB, logger)
 
 	// TODO: DynamoDBを使用してリポジトリを初期化する場合はここに追加
 
 	return &RepositoryFactory{
-		Task: taskRepo,
+		Task:    taskRepo,
+		Session: sessionRepo,
 	}
 }
