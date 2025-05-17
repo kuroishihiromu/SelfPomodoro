0d01654be3c9825cf02c4d1b6f15dcb7247d4d53
tsunakit99 <tainosuke0926@gmail.com>
2025-05-17 22:46:38 +0900

    add: Seesion関連を追加

--- backend/internal/usecase/usecases.go
+++ backend/internal/usecase/usecases.go
@@ -7,16 +7,18 @@ import (
 
 // UseCases はすべてのユースケースをまとめた構造体
 type UseCases struct {
-	Auth AuthUseCase
-	Task TaskUseCase
+	Auth    AuthUseCase
+	Task    TaskUseCase
+	Session SessionUseCase
 	// TODO: 他のユースケースを追加する場合はここにフィールドを追加
 }
 
 // NewUseCases はすべてのユースケースを初期化する
-func NewUseCases(taskRepo repository.TaskRepository, logger logger.Logger) *UseCases {
+func NewUseCases(taskRepo repository.TaskRepository, sessionRepo repository.SessionRepository, logger logger.Logger) *UseCases {
 	return &UseCases{
-		Auth: NewAuthUseCase(logger),
-		Task: NewTaskUseCase(taskRepo, logger),
+		Auth:    NewAuthUseCase(logger),
+		Task:    NewTaskUseCase(taskRepo, logger),
+		Session: NewSessionUseCase(sessionRepo, nil, logger),
 		// TODO: 他のユースケースを初期化する場合はここに追加
 	}
 }
