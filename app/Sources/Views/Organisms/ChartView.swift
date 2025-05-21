a5a9e8871a55c22a77c262102db10dd10dcb2b9b
yoshidashuya <lemon.sy.1221@gmail.com>
2025-05-21 14:00:43 +0900

    add: ChartView.swiftファイルを作成

---
+++ app/Sources/Views/Organisms/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  SelfPomodoro
+//
+//  Created by し on 2025/05/20.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
