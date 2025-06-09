80b334448bc22729121cd53dda912e29d2c89e93
porChe1223 <houshinri@gmail.com>
2025-06-10 06:02:41 +0900

    change: change column name

--- optim/tests/main.py
+++ optim/tests/main.py
@@ -11,13 +11,13 @@ def test_round_optimizer():
     assert isinstance(data["break_time"], (int, float))
 
 def test_session_optimizer():
-    response = client.get("session_optimizer/123e4567-e89b-12d3-a456-426614174000?average_focus_score=100")
+    response = client.get("session_optimizer/123e4567-e89b-12d3-a456-426614174000?avg_focus_score=100")
     assert response.status_code == 200
     data = response.json()
-    assert isinstance(data["session_break_time"], (int, float))
-    assert isinstance(data["number_of_round"], int)
+    assert isinstance(data["break_time"], (int, float))
+    assert isinstance(data["round_count"], int)
     assert isinstance(data["total_work_time"], (int, float))
     # 値の範囲チェック
-    assert 10 <= data["session_break_time"] <= 60
-    assert 1 <= data["number_of_round"] <= 8
+    assert 10 <= data["break_time"] <= 60
+    assert 1 <= data["round_count"] <= 8
     assert 60 <= data["total_work_time"] <= 480
