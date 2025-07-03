4dd83caa244c3564731b7a4d7cc84062abad1c6b
yoshidashuya <lemon.sy.1221@gmail.com>
2025-07-03 21:05:01 +0900

    fix: stateと関数の切り分け

---
+++ app/Sources/Utils/Date+.swift
@@ -0,0 +1,18 @@
+//
+//  Date+.swift
+//  SelfPomodoro
+//
+//  Created by し on 2025/07/03.
+//
+
+import SwiftUI
+
+struct Date_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Date_()
+}
