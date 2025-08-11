c1fb9f33ff2165b5feb882f58360095aba799e26
kuroishihiromu <kuroishihiromu@gmail.com>
2025-08-11 14:27:19 +0900

    fix: PasswordField

--- app/Sources/Views/Atoms/TextFieldView.swift
+++ app/Sources/Views/Atoms/TextFieldView.swift
@@ -52,30 +52,48 @@ struct NormalTextField: View {
     }
 }
 
-#Preview {
-    PreviewWrapperTextField()
-}
 
-struct PreviewWrapperTextField: View {
-    @State var email = ""
-    @State var password = ""
+struct PasswordTextField: View {
+    let placeholder: String
+    let width: CGFloat
+    let height: CGFloat
+    let icon: Image?
+    @Binding var text: String
+
+    private let bgColor = ColorTheme.lightGray
+    private let fontColor = ColorTheme.black
+
+    init(
+        placeholder: String,
+        icon: Image? = nil,
+        width: CGFloat,
+        height: CGFloat,
+        text: Binding<String>
+    ) {
+        self.placeholder = placeholder
+        self.icon = icon
+        self.width = width
+        self.height = height
+        _text = text
+    }
 
     var body: some View {
-        VStack(spacing: 20) {
-            NormalTextField(
-                placeholder: "Your email address",
-                icon: Image(.mail),
-                width: 350,
-                height: 44,
-                text: $email
-            )
-            NormalTextField(
-                placeholder: "Enter your password",
-                icon: Image(.key),
-                width: 350,
-                height: 44,
-                text: $password
-            )
+        HStack(spacing: 10) {
+            if let icon = icon {
+                icon
+                    .resizable()
+                    .frame(width: 18, height: 18)
+                    .foregroundColor(fontColor)
+            }
+
+            SecureField(placeholder, text: $text)
+                .foregroundColor(fontColor)
+                .autocapitalization(.none)
+                .keyboardType(.emailAddress)
         }
+        .padding()
+        .frame(width: width, height: height)
+        .background(bgColor)
+        .cornerRadius(6)
     }
 }
