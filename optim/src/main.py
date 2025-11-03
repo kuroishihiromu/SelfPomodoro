889f29c9b000f804ce350b59535c336dc7eaa36b
Li Fangxin <fangxin.li@nulab.com>
2025-11-04 07:02:35 +0900

    feat: Add API Optimize Round from Requestbody data

--- optim/src/main.py
+++ optim/src/main.py
@@ -1,9 +1,9 @@
-#! /usr/bin/env python3
-
 import uuid
 from fastapi import FastAPI
+from typing import Dict, List, Union
 from app.optimize_round_with_focus_score_prediction import optimize_round_from_dynamodb_data_with_focus_score_prediction
 from app.optimize_session_from_dynamodb_data import optimize_session_from_dynamodb_data
+from app.optimize_round_from_requestbody_data import optimize_round_from_requestbody_data
 
 
 app = FastAPI()
@@ -23,6 +23,7 @@ def round_v1(
     # return _optimize_round_from_csv_data(user_id, focus_score)
     return {"message": "このエンドポイントは非推奨です。/round/v4/{user_id} を使用してください。"}
 
+
 # DynamoDBデータから最適化
 @app.get("/round/v2/{user_id}")
 def round_v2(
@@ -32,6 +33,7 @@ def round_v2(
     # return _optimize_round_from_dynamodb_data(user_id, focus_score)
     return {"message": "このエンドポイントは非推奨です。/round/v4/{user_id} を使用してください。"}
 
+
 # 疲労度とモチベーション予測モデルを使用した最適化
 @app.get("/round/v3/{user_id}")
 def round_v3(
@@ -39,6 +41,16 @@ def round_v3(
     user_input_focus_score: float
 ):
     return optimize_round_from_dynamodb_data_with_focus_score_prediction(user_id, user_input_focus_score)
+
+
+# RequestBodyデータから最適化
+@app.post("/round/v4/{user_id}")
+def round_v4(
+    user_id: uuid.UUID,
+    round_data: List[Dict[str, Union[str, float, int]]]
+):
+    return optimize_round_from_requestbody_data(user_id, round_data)
+
 
 
 # --- セッション最適化 ---
