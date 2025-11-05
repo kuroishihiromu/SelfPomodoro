659e9c73ae5bac0b2a9c995441bbde7fe3271723
tsunakit99 <tainosuke0926@gmail.com>
2025-11-05 17:53:44 +0900

    fix: @MainActor

--- app/Sources/App/Dependency/AppDependencies.swift
+++ app/Sources/App/Dependency/AppDependencies.swift
@@ -9,12 +9,14 @@ import SwiftData
 import ComposableArchitecture
 
 extension DependencyValues {
+    @MainActor
     mutating func configureAppDependencies(modelContainer: ModelContainer) {
         let context = ModelContext(modelContainer)
         self.userRepository = SwiftDataUserRepository(context: context)
         self.taskRepository = SwiftDataTaskRepository(context: context)
     }
 
+    @MainActor
     static func makeConfiguredDependencies(modelContainer: ModelContainer) -> DependencyValues {
         var dependencies = DependencyValues._current
         dependencies.configureAppDependencies(modelContainer: modelContainer)
