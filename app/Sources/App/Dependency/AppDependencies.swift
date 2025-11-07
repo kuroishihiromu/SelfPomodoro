030f85bb5ec45fa8b887c49e147cd34a8b602877
tsunakit99 <tainosuke0926@gmail.com>
2025-11-07 20:21:54 +0900

    add: session. round, userconfig追加

--- app/Sources/App/Dependency/AppDependencies.swift
+++ app/Sources/App/Dependency/AppDependencies.swift
@@ -12,8 +12,17 @@ extension DependencyValues {
     @MainActor
     mutating func configureAppDependencies(modelContainer: ModelContainer) {
         let context = ModelContext(modelContainer)
-        self.userRepository = SwiftDataUserRepository(context: context)
-        self.taskRepository = SwiftDataTaskRepository(context: context)
+        let userRepository = SwiftDataUserRepository(context: context)
+        let taskRepository = SwiftDataTaskRepository(context: context)
+        let sessionRecordRepository = SwiftDataSessionRecordRepository(context: context)
+        let roundRecordRepository = SwiftDataRoundRecordRepository(context: context)
+        let userConfigRepository = SwiftDataUserConfigRepository(context: context)
+
+        self.userRepository = userRepository
+        self.taskRepository = taskRepository
+        self.sessionRecordRepository = sessionRecordRepository
+        self.roundRecordRepository = roundRecordRepository
+        self.userConfigRepository = userConfigRepository
     }
 
     @MainActor
